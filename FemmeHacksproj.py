75e32e60be48821b2f42894ad713e94b2a5a8823
Shayne Rothman <shrothman@Shaynes-MacBook-Air.local>
2019-02-09 11:27:03 -0500

    here

--- FemmeHacksproj.py
+++ FemmeHacksproj.py
@@ -6,10 +6,16 @@ Vicky Liu
 Arin Wheeler
 """
 
+# Can contact emergency contact after individual breaches limit
+# Call uber button
+# interactions with certain
+
 def main():
     # TODO: Print starting info here
     weight = int(input("Enter your weight: "))
     height = int(input("Enter your height (in inches): "))
+    print("heyo im Shayne wassap")
+
 
 
 
