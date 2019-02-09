6a05cb204f4df154a5aea015453e2f92c0dc917e
Shayne Rothman <shrothman@Shaynes-MacBook-Air.local>
2019-02-09 11:02:08 -0500

    starter code

--- FemmeHacksproj.py
+++ FemmeHacksproj.py
@@ -5,3 +5,13 @@ Shiyi Lin
 Vicky Liu
 Arin Wheeler
 """
+
+def main():
+    # TODO: Print starting info here
+    weight = int(input("Enter your weight: "))
+    height = int(input("Enter your height (in inches): "))
+
+
+
+
+main()
